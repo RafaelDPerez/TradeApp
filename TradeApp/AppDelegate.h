8299664be585d4bcfff7de0223ca11c520fe2a42
Rafael Perez <rafaelperez@rafaels-macbook-pro.local>
2016-09-24 18:09:04 -0400

    Initial Commit

---
+++ TradeApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TradeApp
+//
+//  Created by Rafael Perez on 9/24/16.
+//  Copyright © 2016 Rafael Perez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TradeApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TradeApp
+//
+//  Created by Rafael Perez on 9/24/16.
+//  Copyright © 2016 Rafael Perez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
